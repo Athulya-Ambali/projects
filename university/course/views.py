ed171a928379e1e5d2a584c5cd3f856ab121447f
Athulya-Ambali <athulyaambali885@gmail.com>
2023-09-19 14:33:13 +0530

    initial commit

---
+++ university/course/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from course.models import *
+
+# Create your views here.
+
+def display(request):
+    obj=Coursetb.objects.all()
+    return render(request,'courseview.html',{'data':obj})   
+
+
+
+
+
+def show(request):
+    obj=Syllabustb.objects.all()
+    return render(request,'syllabusview.html',{'data':obj})
